42b280d3b70f40d8a238d9b2fa4c3e0d726a7c75
henderson2k <henderson2k@users.noreply.github.com>
2023-03-17 23:03:10 +0000

    Update RunFromGithub.ps1

--- RunFromGithub.ps1
+++ RunFromGithub.ps1
@@ -1,4 +1,4 @@
-# run scripts (link RAW) from Github
+# run scripts (link RAW file) from Github
 
 $ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/henderson2k/sn/main/popup.ps1
 Invoke-Expression $($ScriptFromGitHub.Content)
