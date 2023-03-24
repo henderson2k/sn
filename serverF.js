2ae28ef1bb485b37815a9e6f024befd1f13ac682
henderson2k <henderson2k@users.noreply.github.com>
2023-03-24 10:22:59 +0000

    Update serverF.js

--- serverF.js
+++ serverF.js
@@ -384,7 +384,7 @@ elem.execCommand("Copy");
 
     <div style="position: absolute; right: 0px; padding-right: 160px; padding-bottom: 20px" id="qrcode"></div>
 
-<a>Hello from github version  of this file</a>
+<a>Hello from github version  of this file2</a>
     <script type="text/javascript">
 new QRCode(document.getElementById("qrcode"), "${callettel}");
 </script>
