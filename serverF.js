07bc1dbb30ca8bc376dc666bcaafd55bd2c1dcb7
henderson2k <henderson2k@users.noreply.github.com>
2023-04-03 14:53:13 +0100

    Update serverF.js

--- serverF.js
+++ serverF.js
@@ -10,7 +10,7 @@ async function getPage1() {
   Page1 = await res.text();
 Page1 = Page1
 .replace('${date1}','ANDREWSH')
-console.log("update f")
+console.log("update from ASH")
 //console.log(Page1)
 }
 
