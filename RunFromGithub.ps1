7bb2a8efd318b8dd1628642769b9e56bcbe321b5
henderson2k <henderson2k@users.noreply.github.com>
2023-04-03 14:30:38 +0100

    Deleting RunFromGithub.ps1

--- RunFromGithub.ps1
+++
@@ -1,4 +0,0 @@
-# run scripts (link RAW file) from Github
-
-$ScriptFromGitHub = Invoke-WebRequest https://raw.githubusercontent.com/henderson2k/sn/main/popup.ps1
-Invoke-Expression $($ScriptFromGitHub.Content)
