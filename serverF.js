b6f438cebeeca6dbb87c4110181f5f18c339a7f5
henderson2k <henderson2k@users.noreply.github.com>
2023-04-04 10:17:17 +0100

    Update serverF.js

--- serverF.js
+++ serverF.js
@@ -2,19 +2,7 @@
 // set-executionpolicy remotesigned
 //   Set-ExecutionPolicy unrestricted
 
-  async function getPage1() {
-    //  let obj;
-    
-      const res = await fetch('https://raw.githubusercontent.com/henderson2k/sn/main/page1.html')
-    
-      Page1 = await res.text();
-    //Page1 = obj
-    //.replace('${date1}','ANDREWSH')
-    //console.log(Page1)
-    }
-    
-    getPage1()
-    
+  
     
     let objectDate = new Date();
     
