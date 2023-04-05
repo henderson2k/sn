74c9782249f0e82ddb8862157f5edf74d07fb762
henderson2k <henderson2k@users.noreply.github.com>
2023-04-05 20:52:59 +0100

    Update stub.js

--- stub.js
+++ stub.js
@@ -15,7 +15,7 @@ main()
 function main() {
 
   //  if (url.indexOf('nav_to.do') > 0) {
-        console.log('ServiceNow full')
+  //      console.log('ServiceNow full')
 
    //   var mdl = document.getElementsByClassName('modal-body');
         var ifrm = document.getElementById('gsft_main')
