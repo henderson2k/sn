da1f95108086f1d3909b370c46218dc63e4a84c5
henderson2k <henderson2k@users.noreply.github.com>
2024-03-07 22:18:16 +0000

    Update ek.js

---
+++ ek.js
@@ -0,0 +1,35 @@
+
+fetch("https://api.stylelink.ergotron.com/signin", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://stylelink.ergotron.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": null,
+  "method": "OPTIONS",
+  "mode": "cors",
+  "credentials": "omit"
+}); ;
+fetch("https://api.stylelink.ergotron.com/signin", {
+  "headers": {
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json",
+    "sec-ch-ua": "\"Not A(Brand\";v=\"99\", \"Google Chrome\";v=\"121\", \"Chromium\";v=\"121\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"Chrome OS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site"
+  },
+  "referrer": "https://stylelink.ergotron.com/",
+  "referrerPolicy": "strict-origin-when-cross-origin",
+  "body": "{\"email\":\"andrew.henderson2@lanarkshire.scot.nhs.uk\",\"password\":\"1\"}",
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+});
