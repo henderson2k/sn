50eda81d57e771503c7bce1408645ace9966529b
henderson2k <henderson2k@users.noreply.github.com>
2024-03-11 15:57:43 +0000

    Update xat.js

--- xat.js
+++ xat.js
@@ -3,7 +3,7 @@
 if (accessToken == null) {
     const accessToken = localStorage.getItem('ERG__TOKEN');
 } else {
-    console.log("The value is not null.");
+    const accessToken = localStorage.getItem('ERG__TOKEN');
 }
 
 
