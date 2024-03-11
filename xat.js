460e48c2e8ea99b98e7b5343a1a59ef01eca32b8
henderson2k <henderson2k@users.noreply.github.com>
2024-03-11 15:58:18 +0000

    Update xat.js

--- xat.js
+++ xat.js
@@ -3,7 +3,6 @@ const accessToken = localStorage.getItem('ERG__TOKEN');
 if (accessToken == null) {
     const accessToken = sessionStorage.getItem('ERG__TOKEN');
 } else {
-    const accessToken = localStorage.getItem('ERG__TOKEN');
 }
 
     navigator.clipboard.writeText(accessToken);
