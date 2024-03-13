d8120426c491f2004c0972db314a15b34b7f13c5
henderson2k <henderson2k@users.noreply.github.com>
2024-03-13 14:19:32 +0000

    Update xat.js

--- xat.js
+++ xat.js
@@ -2,8 +2,7 @@ const accessToken = sessionStorage.getItem('ERG__TOKEN') || localStorage.getItem
 
 if (accessToken) {
     navigator.clipboard.writeText(accessToken)
-        .then(() => console.log('Access token copied to clipboard'))
-        .catch(error => console.error('Error copying access token to clipboard:', error));
+     
 } else {
     console.error('Access token not found in sessionStorage or localStorage');
 }
