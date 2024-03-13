4bd5bc2a9c69b9861041583cf030414d74126bf3
henderson2k <henderson2k@users.noreply.github.com>
2024-03-13 14:07:08 +0000

    Update xat.js

--- xat.js
+++ xat.js
@@ -1,6 +1,6 @@
 const accessToken = localStorage.getItem('ERG__TOKEN');
 
-if (accessToken.length && accessToken.length < 10) {
+if (accessToken === null) {
     const accessToken = sessionStorage.getItem('ERG__TOKEN');
 } else {
 }
