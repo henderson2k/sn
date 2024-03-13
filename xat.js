6c1d6d03f714fcde8393f9295aa0236449a40776
henderson2k <henderson2k@users.noreply.github.com>
2024-03-13 14:19:40 +0000

    Update xat.js

--- xat.js
+++ xat.js
@@ -1,8 +1,7 @@
 const accessToken = sessionStorage.getItem('ERG__TOKEN') || localStorage.getItem('ERG__TOKEN');
 
 if (accessToken) {
-    navigator.clipboard.writeText(accessToken)
- 
+    navigator.clipboard.writeText(accessToken) 
 } else {
     console.error('Access token not found in sessionStorage or localStorage');
 }
