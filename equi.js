9c933d13a29d5511e72b3259b2668babc7422bc4
henderson2k <henderson2k@users.noreply.github.com>
2024-05-21 15:46:16 +0100

    Update equi.js

--- equi.js
+++ equi.js
@@ -1,4 +1,6 @@
-function getCookieValue(name) {
+f
+
+unction getCookieValue(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
     if (parts.length === 2) return parts.pop().split(';').shift();
