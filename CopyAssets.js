625107686d7f35163e809748928deadd8777eaa8
henderson2k <henderson2k@users.noreply.github.com>
2024-06-11 14:29:13 +0100

    Update CopyAssets.js

---
+++ CopyAssets.js
@@ -0,0 +1,22 @@
+setTimeout(function() {
+    (function() {
+        var matchesSet = new Set();
+        var elements = document.querySelectorAll('body *');
+        elements.forEach(function(element) {
+            var text = element.innerText.trim();
+            var regex = /[DM]\d{5}/g;
+            var match;
+            while ((match = regex.exec(text)) !== null) {
+                matchesSet.add(match[0]);
+            }
+        });
+        var allMatches = Array.from(matchesSet).join('\n');
+        navigator.clipboard.writeText(allMatches).then(function() {
+            console.log(allMatches);
+            // alert('Unique matches copied to clipboard!');
+        }).catch(function(error) {
+            console.error('Error copying text: ', error);
+            alert('Error copying text. See console for details.');
+        });
+    })();
+}, 5000);
