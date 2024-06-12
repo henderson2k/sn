f6eea77453183a64be18c01a88ff19b8e94272c6
henderson2k <henderson2k@users.noreply.github.com>
2024-06-12 10:11:33 +0100

    Update CopyAssets.js

--- CopyAssets.js
+++ CopyAssets.js
@@ -1,4 +1,14 @@
+const urlToCheck = 'https://stylelink.ergotron.com/app/carts?org=354';
 
+// Regular expression pattern for the specified URL
+const specifiedURLPattern = /^https:\/\/stylelink\.ergotron\.com\/app\/carts\?org=354(.*)/;
+
+// Check if the URL matches the specified pattern
+if (specifiedURLPattern.test(urlToCheck)) {
+    console.log('URL matches the specified pattern.');
+} else {
+    console.log('URL does not match the specified pattern.');
+}
 
 
 
