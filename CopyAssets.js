f6b6c09ff13309870420e9e010ef6f214b318587
henderson2k <henderson2k@users.noreply.github.com>
2024-06-12 10:12:02 +0100

    Update CopyAssets.js

--- CopyAssets.js
+++ CopyAssets.js
@@ -48,7 +48,7 @@ if (specifiedURLPattern.test(urlToCheck)) {
     });
 })();
 } else {
-    console.log('URL does not match the specified pattern.');
+    alert URL does not match the specified pattern.');
 }
 
 
